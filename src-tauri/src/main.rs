5d1694d5935193b4e3dfb2624cfe3c5ae1de367a
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2026-03-22 03:48:09 +0800

    fix: hide console window in release builds, bump version to v0.1.1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents an additional console window on Windows in release builds.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     slaysp2manager_lib::run();
 }
